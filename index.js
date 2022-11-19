e5a7ce2230082364d8b7e922e699b6311e502007
Katie <katiefaybehrmann@gmail.com>
2022-11-19 13:42:00 -0700

    commit tree 8cecb8c9b72f39fd277c59c115eb3467173bb843

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Add your code here
+function submitData(userName, userEmail) {
+    return fetch("http://localhost:3000/users", 
+    {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({name: userName, email: userEmail})
+    })
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (object) {
+            console.log(object);
+            let ident = document.createElement('h4')
+            ident.textContent = object["id"]
+            document.body.appendChild(ident)
+        })
+        .catch (function (error){
+            let iErred = document.createElement('h4')
+            iErred.textContent = "Unauthorized Access"
+            document.body.appendChild(iErred)
+            console.log(error.message)
+        })
+}
